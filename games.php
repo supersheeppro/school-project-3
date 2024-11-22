a460af5dd040617f7a4a3852679cbf89ded5a351
deviv <devivanriet@gmail.com>
2024-11-23 00:32:18 +0100

    fzdvgvb

--- games.php
+++ games.php
@@ -57,37 +57,85 @@
             <figcaption>SPORT</figcaption>
         </figure>
     </section>
+    <hr>
 
     <!-- Sectie met zoekbalk -->
     <section class="all-games">
-        <div class="header">
-            <h2 class="title">All Games</h2>
-            <input type="text" class="search-bar" placeholder="Search...">
+        <div class="header-games">
+            <h1>All Games</h1>
+            <form id="form">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                <input type="search" id="query" name="q" placeholder="Search">
+            </form>
         </div>
         <div class="game-list">
             <figure>
-                <img src="images/minecraft.jpg" alt="Game 7">
+                <img src="images/battlefront.jpg" alt="Game 7">
                 <figcaption>Game 7</figcaption>
             </figure>
             <figure>
-                <img src="images/minecraft.jpg" alt="Game 8">
+                <img src="images/battlefront.jpg"  alt="Game 8">
                 <figcaption>Game 8</figcaption>
             </figure>
             <figure>
-                <img src="images/minecraft.jpg" alt="Game 9">
+                <img src="images/battlefront.jpg"  alt="Game 9">
                 <figcaption>Game 9</figcaption>
             </figure>
             <figure>
-                <img src="images/minecraft.jpg" alt="Game 10">
+                <img src="images/battlefront.jpg"  alt="Game 10">
                 <figcaption>Game 10</figcaption>
             </figure>
             <figure>
-                <img src="images/minecraft.jpg" alt="Game 11">
+                <img src="images/battlefront.jpg" alt="Game 11">
+                <figcaption>Game 11</figcaption>
+            </figure>
+        </div>
+        <div class="game-list">
+            <figure>
+                <img src="images/battlefront.jpg" alt="Game 7">
+                <figcaption>Game 7</figcaption>
+            </figure>
+            <figure>
+                <img src="images/battlefront.jpg"  alt="Game 8">
+                <figcaption>Game 8</figcaption>
+            </figure>
+            <figure>
+                <img src="images/battlefront.jpg"  alt="Game 9">
+                <figcaption>Game 9</figcaption>
+            </figure>
+            <figure>
+                <img src="images/battlefront.jpg"  alt="Game 10">
+                <figcaption>Game 10</figcaption>
+            </figure>
+            <figure>
+                <img src="images/battlefront.jpg" alt="Game 11">
+                <figcaption>Game 11</figcaption>
+            </figure>
+        </div>
+        <div class="game-list">
+            <figure>
+                <img src="images/battlefront.jpg" alt="Game 7">
+                <figcaption>Game 7</figcaption>
+            </figure>
+            <figure>
+                <img src="images/battlefront.jpg"  alt="Game 8">
+                <figcaption>Game 8</figcaption>
+            </figure>
+            <figure>
+                <img src="images/battlefront.jpg"  alt="Game 9">
+                <figcaption>Game 9</figcaption>
+            </figure>
+            <figure>
+                <img src="images/battlefront.jpg"  alt="Game 10">
+                <figcaption>Game 10</figcaption>
+            </figure>
+            <figure>
+                <img src="images/battlefront.jpg" alt="Game 11">
                 <figcaption>Game 11</figcaption>
             </figure>
         </div>
         <div class="pagination">
-            <i class="fa fa-arrow-left"></i>
+            <i class="fa fa-facebook"></i>
             <span>Page 1/4</span>
             <i class="fa fa-arrow-right"></i>
         </div>
