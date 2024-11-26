90f641b9eb1aefa93364f00579e9ff34cbe270ef
deviv <devivanriet@gmail.com>
2024-11-26 12:21:48 +0100

    laatste dingentjes

--- js/script.js
+++ js/script.js
@@ -9,25 +9,19 @@ const images = [
 let currentImageIndex = 0;
 
 function changeBackground() {
-    // Bereken de volgende afbeelding
     const nextImageIndex = (currentImageIndex + 1) % images.length;
-
-    // Pak het slideshow-element
     const slideshow = document.querySelector('.content-slideshow');
 
-    // Stel de volgende afbeelding in op ::after
     slideshow.style.setProperty('--next-slideshow-bg', `url("${images[nextImageIndex]}")`);
 
-    // Start de fade-in: de ::after wordt zichtbaar
     slideshow.classList.add('fade-in');
 
-    // Na 1.5 seconden (transitieduur), wissel de actieve afbeelding
     setTimeout(() => {
         slideshow.style.setProperty('--slideshow-bg', `url("${images[nextImageIndex]}")`);
         slideshow.classList.remove('fade-in');
-        currentImageIndex = nextImageIndex; // Update de huidige index
-    }, 1500); // Moet overeenkomen met de `transition`-tijd in CSS
+        currentImageIndex = nextImageIndex;
+    }, 1500);
 }
 
-// Start de slideshow
-setInterval(changeBackground, 5000); // Elke 5 seconden
+
+setInterval(changeBackground, 5000);
